3eb8d132fe72da673542144acc251676511d9992
riku <riku.suny@gmail.com>
2021-07-07 16:50:29 +0900

    react-navigationの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Text, SafeAreaView } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
 
 export const App = () => {
   return (
     <SafeAreaProvider>
-      <SafeAreaView>
+      <NavigationContainer>
         <Text>Hello RN</Text>
-      </SafeAreaView>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 };
