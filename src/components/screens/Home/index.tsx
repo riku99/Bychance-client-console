1ebae75f8c4bc391eded518961212742e0aa517c
riku <riku.suny@gmail.com>
2021-07-11 11:53:01 +0900

    Secitonの作成

--- src/components/screens/Home/index.tsx
+++ src/components/screens/Home/index.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
+import { Section } from "~/components/utils/Section";
+
 export const Home = React.memo(() => {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Section style={{ height: 100, marginTop: 100 }}>
+        <View />
+      </Section>
+    </View>
+  );
 });
 
 const styles = StyleSheet.create({
---
+++ src/components/utils/Section/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, StyleSheet, ViewStyle } from "react-native";
+
+type Props = {
+  children: React.ReactNode;
+  style?: ViewStyle;
+};
+
+export const Section = React.memo(({ children, style }: Props) => {
+  return <View style={[styles.container, { ...style }]}>{children}</View>;
+});
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+  },
+});
