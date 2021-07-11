e0c78455ab4b93386bde490fc138f4824b486afa
riku <riku.suny@gmail.com>
2021-07-11 12:01:42 +0900

    分割

---
+++ src/components/screens/Home/Profile.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+import { Section } from "~/components/utils/Section";
+
+export const Profile = React.memo(() => {
+  return (
+    <Section style={{ height: 100 }}>
+      <View />
+    </Section>
+  );
+});
--- src/components/screens/Home/index.tsx
+++ src/components/screens/Home/index.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 
 import { Section } from "~/components/utils/Section";
+import { Profile } from "./Profile";
 
 export const Home = React.memo(() => {
   return (
     <View style={styles.container}>
-      <Section style={{ height: 100, marginTop: 100 }}>
-        <View />
-      </Section>
+      <View style={styles.profileContainer}>
+        <Profile />
+      </View>
     </View>
   );
 });
@@ -17,5 +18,8 @@ const styles = StyleSheet.create({
   container: {
     height: "100%",
     width: "100%",
+  },
+  profileContainer: {
+    marginTop: 25,
   },
 });
