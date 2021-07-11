7d7ab7adeef29b0f533f2ac05ea19aa2752a38d2
riku <riku.suny@gmail.com>
2021-07-11 17:59:46 +0900

    add setValue

--- src/components/screens/UserEdit/index.tsx
+++ src/components/screens/UserEdit/index.tsx
@@ -1,21 +1,41 @@
 import React, { useState } from "react";
-import { View, StyleSheet, ViewStyle, TextInput, Text } from "react-native";
+import {
+  View,
+  StyleSheet,
+  ViewStyle,
+  TextInput,
+  Text,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import { shallowEqual, useSelector } from "react-redux";
 
 import { ProfileImage } from "./ProfileImage";
 import { Section } from "~/components/utils/Section";
+import { RootState } from "~/stores";
 
 type _Props = {
   style?: ViewStyle;
   title: string;
   value?: string | null;
+  setValue: (v: string) => void;
 };
-const EidtItem = React.memo(({ style, title, value }: _Props) => {
+const EidtItem = React.memo(({ style, title, value, setValue }: _Props) => {
+  const onAddressPress = () => {
+    if (title === "住所") {
+      console.log("address");
+    }
+  };
+
   return (
     <View style={styles.EditItem}>
       <Text style={styles.EditItemTitle}>{title}</Text>
       <TextInput
         defaultValue={value ? value : undefined}
         style={styles.EditItemInput}
+        editable={title === "住所" ? false : true}
+        onPressOut={onAddressPress}
+        onChangeText={setValue}
       />
     </View>
   );
@@ -24,19 +44,47 @@ const EidtItem = React.memo(({ style, title, value }: _Props) => {
 export const UserEdit = React.memo(() => {
   const [imageUri, setImageUri] = useState("");
 
+  const user = useSelector(
+    (state: RootState) => state.usersReducer.user!,
+    shallowEqual
+  );
+
+  const [name, setName] = useState(user.name);
+  const [address, setAddress] = useState(user.address);
+  const [url, setUrl] = useState(user.url);
+  const [instagram, setInstagram] = useState(user.instagram);
+  const [twitter, setTwitter] = useState(user.twitter);
+
   return (
-    <View style={styles.container}>
-      <Section style={styles.section}>
-        <ProfileImage imageUri={imageUri} setImageUri={setImageUri} />
-        <View style={styles.EditItemsContaner}>
-          <EidtItem title="名前" value="riku" key="名前" />
-          <EidtItem title="住所" key="住所" />
-          <EidtItem title="URL" key="URL" />
-          <EidtItem title="instagram" key="instagram" />
-          <EidtItem title="twitter" key="twitter" />
-        </View>
-      </Section>
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <Section style={styles.section}>
+          <ProfileImage imageUri={imageUri} setImageUri={setImageUri} />
+          <View style={styles.EditItemsContaner}>
+            <EidtItem title="名前" value={name} key="名前" setValue={setName} />
+            <EidtItem
+              title="住所"
+              value={address}
+              key="住所"
+              setValue={setAddress}
+            />
+            <EidtItem title="URL" value={url} key="URL" setValue={setUrl} />
+            <EidtItem
+              title="instagram"
+              value={instagram}
+              key="instagram"
+              setValue={setInstagram}
+            />
+            <EidtItem
+              title="twitter"
+              value={twitter}
+              key="twitter"
+              setValue={setTwitter}
+            />
+          </View>
+        </Section>
+      </View>
+    </TouchableWithoutFeedback>
   );
 });
 
@@ -46,7 +94,7 @@ const styles = StyleSheet.create({
     height: "100%",
   },
   section: {
-    marginTop: 5,
+    marginTop: 15,
     alignItems: "center",
     paddingTop: 15,
     paddingBottom: 15,
@@ -58,7 +106,7 @@ const styles = StyleSheet.create({
   },
   EditItem: {
     flexDirection: "row",
-    width: "70%",
+    width: "80%",
     marginTop: 30,
   },
   EditItemTitle: {
@@ -70,8 +118,7 @@ const styles = StyleSheet.create({
   EditItemInput: {
     marginLeft: 40,
     fontSize: 15,
-    width: "80%",
-
+    width: "70%",
     borderBottomColor: "lightgray",
     borderBottomWidth: 1,
   },
