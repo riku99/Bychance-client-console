2b4ec08e5580d0e55db2fdb0733af0c1fd2d54e1
riku <riku.suny@gmail.com>
2021-07-15 19:11:27 +0900

    レイアウトの追加

--- src/components/screens/Posts/index.tsx
+++ src/components/screens/Posts/index.tsx
@@ -1,13 +1,44 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 export const Posts = React.memo(() => {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <SafeAreaView>
+        <ScrollView contentContainerStyle={styles.container}>
+          <View style={styles.contents}>
+            <View style={styles.mainSection}></View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </View>
+  );
 });
 
 const styles = StyleSheet.create({
   container: {
     width: "100%",
     height: "100%",
+    backgroundColor: "white",
+  },
+  contents: {
+    width: "100%",
+    height: "100%",
+    paddingHorizontal: 10,
+    marginTop: 20,
+  },
+  mainSection: {
+    height: "40%",
+    width: "100%",
+    backgroundColor: "white",
+    borderRadius: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 3,
   },
 });
