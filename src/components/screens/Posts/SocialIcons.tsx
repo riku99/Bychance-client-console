34313974c4ffceb9bf48ac4fc97bcd297bdc96fd
riku <riku.suny@gmail.com>
2021-07-16 19:09:35 +0900

    linkingの追加

--- src/components/screens/Posts/SocialIcons.tsx
+++ src/components/screens/Posts/SocialIcons.tsx
@@ -1,8 +1,28 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Alert, Linking } from "react-native";
 import { SocialIcon } from "react-native-elements";
 
+const notSupportedAlert = () => {
+  Alert.alert("無効なURLです", "");
+};
+
 export const SocialIcons = React.memo(() => {
+  const onPress = async () => {
+    try {
+      const supported = await Linking.canOpenURL(
+        "https://www.instagram.com/rik0999/"
+      );
+
+      if (supported) {
+        await Linking.openURL("https://www.instagram.com/rik0999/");
+      } else {
+        notSupportedAlert();
+      }
+    } catch (e) {
+      notSupportedAlert();
+    }
+  };
+
   return (
     <View style={styles.icons}>
       <SocialIcon
@@ -10,11 +30,13 @@ export const SocialIcons = React.memo(() => {
         style={{ width: 30, height: 30, backgroundColor: "pink" }}
         iconSize={17}
         underlayColor="pink"
+        onPress={onPress}
       />
       <SocialIcon
         type="twitter"
         style={{ width: 30, height: 30 }}
         iconSize={17}
+        onPress={onPress}
       />
     </View>
   );
