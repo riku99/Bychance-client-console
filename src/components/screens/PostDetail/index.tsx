90ebd4ffcc75fd95ac125868702f863fee599ccc
riku <riku.suny@gmail.com>
2021-07-17 19:35:53 +0900

    レイアウトの追加

--- src/components/screens/PostDetail/index.tsx
+++ src/components/screens/PostDetail/index.tsx
@@ -1,8 +1,15 @@
 import React from "react";
-import { View, StyleSheet, ScrollView } from "react-native";
-import { Text } from "react-native-elements";
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import { Avatar } from "react-native-elements";
 
 import { Images } from "./Images";
+import { SocialIcons } from "./SoicialIcons";
 
 export const PostDetail = React.memo(() => {
   return (
@@ -15,6 +22,29 @@ export const PostDetail = React.memo(() => {
           <Text style={styles.title}>
             ラテの美味しいオシャレなカフェでゆったりしませんか??
           </Text>
+          <View style={styles.imageAndNameContainer}>
+            <Avatar
+              source={{
+                uri: "https://scontent-nrt1-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/218410191_355142425985263_6733373022815732229_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com&_nc_cat=107&_nc_ohc=x1rPnadrd3oAX9KaDsm&edm=AP_V10EBAAAA&ccb=7-4&oh=1648087381dfc97a26a9b3b1e095beca&oe=60F8EF40&_nc_sid=4f375e",
+              }}
+              rounded
+            />
+            <Text style={styles.name}>cafe newTokyo</Text>
+          </View>
+          <Text style={styles.text}>
+            浅草駅から徒歩5分の場所にあるカフェです。3ヶ月ほど前にオープンしたばかりなので店内はすごく綺麗です!✨
+            {`\n`}
+            今大人気の抹茶ラテをアイスで飲むのがおすすめです😆
+            {`\n`}
+            この画面を表示して頂いた場合ドリンク1杯100円引きになります!
+            {`\n`}※ 1回の表示で複数人使うことはできません。1人1回までです。
+          </Text>
+          <TouchableOpacity activeOpacity={1}>
+            <Text style={styles.url}>https://reactnative.dev/</Text>
+          </TouchableOpacity>
+          <View style={styles.socialIcons}>
+            <SocialIcons iconWidth={35} iconHeight={35} iconSize={19} />
+          </View>
         </View>
       </ScrollView>
     </View>
@@ -36,8 +66,29 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     marginTop: 30,
   },
+  imageAndNameContainer: {
+    flexDirection: "row",
+    marginTop: 20,
+    alignItems: "center",
+  },
+  name: {
+    fontSize: 15,
+    marginLeft: 15,
+  },
   title: {
     fontWeight: "bold",
     fontSize: 22,
+  },
+  text: {
+    marginTop: 20,
+  },
+  url: {
+    marginTop: 15,
+    textDecorationLine: "underline",
+    color: "#52b1fa",
+  },
+  socialIcons: {
+    marginTop: 18,
+    width: 120,
   },
 });
--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const origin = "http:/10.4.180.239:4001";
+export const origin = "http:/localhost:4001";
 
 export const baseUrl = `${origin}/api/v1`;
