d3af57f3165a7a3d41ab8ca48bcfe3574347dcd2
riku <riku.suny@gmail.com>
2021-07-18 07:58:10 +0900

    マップ周りの修正

--- src/components/screens/PostDetail/Map.tsx
+++ src/components/screens/PostDetail/Map.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Platform, PermissionsAndroid } from "react-native";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 
 export const Map = React.memo(() => {
+  const region = useMemo(() => {
+    return {
+      latitude: 35.66263439775389,
+      longitude: 139.70312813997703,
+      latitudeDelta: 0.005,
+      longitudeDelta: 0.005,
+    };
+  }, []);
+
   return (
     <MapView
+      region={region}
       showsUserLocation
       style={{ width: "100%", height: "100%" }}
       provider={Platform.OS === "android" ? PROVIDER_GOOGLE : undefined}
@@ -15,6 +25,10 @@ export const Map = React.memo(() => {
             )
           : "";
       }}
-    />
+    >
+      <Marker
+        coordinate={{ latitude: region.latitude, longitude: region.longitude }}
+      />
+    </MapView>
   );
 });
