9b8ef3ab24ce6a43bbf052e537d7582d1d180834
riku <riku.suny@gmail.com>
2021-07-22 15:32:45 +0900

    fix component

--- src/components/screens/Settings/index.tsx
+++ src/components/screens/Settings/index.tsx
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { View, StyleSheet } from "react-native";
+import { ListItem, Avatar } from "react-native-elements";
 
 export const Settings = React.memo(() => {
-  return <View></View>;
+  const list = useMemo(() => {
+    return [
+      {
+        title: "ログアウト",
+        color: "red",
+        style: { color: "red" },
+      },
+    ];
+  }, []);
+  return (
+    <View style={styles.container}>
+      <View style={styles.contents}>
+        {list.map((l, i) => (
+          <ListItem key={i} bottomDivider>
+            <ListItem.Content>
+              <ListItem.Title style={l.style}>{l.title}</ListItem.Title>
+            </ListItem.Content>
+          </ListItem>
+        ))}
+      </View>
+    </View>
+  );
+});
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+  },
+  contents: {
+    marginTop: 20,
+  },
 });
