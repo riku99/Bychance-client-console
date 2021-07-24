4666ee768c03bbea884bee9d72b8ecd1f32c453b
riku <riku.suny@gmail.com>
2021-07-24 13:14:38 +0900

    クーポンの説明追加

--- src/components/screens/CreatePost/CouponDesc.tsx
+++ src/components/screens/CreatePost/CouponDesc.tsx
@@ -10,7 +10,24 @@ type Props = {
 
 export const CouponDesc = React.memo(({ isVisible, setIsVisible }: Props) => (
   <CustomModal isVisible={isVisible} setIsVisible={setIsVisible}>
-    <View style={styles.contents}></View>
+    <View style={styles.contents}>
+      <Text>
+        この投稿をクーポンとして使えるようにする場合「あり」にしてください。
+        <Text style={{ fontWeight: "bold" }}>✨クーポンあり✨</Text>
+        が表示されてユーザーがクーポンの存在を一目でわかるようになります。
+        {"\n"}
+        {"\n"}
+        「この投稿をクーポンとして使えるようにする場合」とは例えば本文に
+        {"\n"}
+        {"\n"}
+        <Text style={{ fontWeight: "bold" }}>
+          この画面を表示していただいた場合お一人様100円引きさせて頂きます。なお、お1人様1回限りとさせていただきます。
+        </Text>
+        {"\n"}
+        {"\n"}
+        のような文を入れてクーポンとして使えるようにする場合を指します。
+      </Text>
+    </View>
   </CustomModal>
 ));
 
@@ -18,6 +35,7 @@ const styles = StyleSheet.create({
   contents: {
     backgroundColor: "white",
     width: "90%",
-    height: 200,
+    height: 280,
+    padding: 10,
   },
 });
