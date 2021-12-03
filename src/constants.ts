731ea9fc8a781b916b1ecea711de7316798b6719
riku <riku.suny@gmail.com>
2021-12-04 00:40:08 +0900

    fix origin

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
+import Config from "react-native-config";
 export const origin = "http://localhost:4001";
-export const baseUrl = `${origin}/api/v1`;
+export const baseUrl = `${Config.ORIGIN}/api/v1`;
